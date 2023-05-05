6b30158691a553e3beb0fcf1b170b115109b1ca2
Thalvaner Dolabella <thalvanerdolabella2@gmail.com>
2023-05-05 18:34:17 -0300

    Lógica ícone de pesquisa;

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,25 @@ import PropTypes from 'prop-types';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
-function Header({ title }) {
+function Header({ title, search }) {
+  if (search) {
+    return (
+      <div>
+        <img src={ profileIcon } alt="" data-testid="profile-top-btn" />
+        <img src={ searchIcon } alt="" data-testid="search-top-btn" />
+        <h1 data-testid="page-title">{ title }</h1>
+      </div>
+    );
+  }
   return (
     <div>
       <img src={ profileIcon } alt="" data-testid="profile-top-btn" />
-      <img src={ searchIcon } alt="" data-testid="search-top-btn" />
       <h1 data-testid="page-title">{ title }</h1>
     </div>
   );
 }
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+  search: PropTypes.bool.isRequired,
+};
