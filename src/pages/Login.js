0089b9428b8b3877134e2be730aab1f890eba737
devRenatoRodrigues <rehrlz@hotmail.com>
2023-05-05 18:14:42 -0300

    [GROUP 9]-REQ-04

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,43 @@
 import React from 'react';
+import useGetFormsContext from '../hooks/useGetFormContext';
 
 function Login() {
+  const {
+    email,
+    password,
+    isValid,
+    handlePassword,
+    handleEmail } = useGetFormsContext();
+
   return (
-    <div />
+    <form>
+      <input
+        type="email"
+        id="email-input"
+        value={ email }
+        data-testid="email-input"
+        placeholder="Insira seu email"
+        onChange={ ({ target: { value } }) => handleEmail(value) }
+      />
+      <input
+        type="password"
+        id="password-input"
+        value={ password }
+        data-testid="password-input"
+        placeholder="Insira seu password"
+        onChange={ ({ target: { value } }) => handlePassword(value) }
+      />
+      <button
+        id="submit-btn"
+        value="email"
+        data-testid="login-submit-btn"
+        disabled={ isValid }
+      >
+        Entrar
+
+      </button>
+
+    </form>
   );
 }
 
