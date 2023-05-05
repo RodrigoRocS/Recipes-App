801e796ff2a0c30261611c0150ba7cbbb4e35795
Thalvaner Dolabella <thalvanerdolabella2@gmail.com>
2023-05-05 20:06:18 -0300

    Lógica do botão de search

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,54 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header({ title, search }) {
+  const [searchBar, setSearchBar] = useState(false);
+  const handleClick = () => {
+    setSearchBar(!searchBar ? searchBar : !searchBar);
+  };
+
   if (search) {
     return (
       <div>
-        <img src={ profileIcon } alt="" data-testid="profile-top-btn" />
-        <img src={ searchIcon } alt="" data-testid="search-top-btn" />
+        <Link to="/">
+          <img src={ profileIcon } alt="" data-testid="profile-top-btn" />
+        </Link>
+        { searchBar }
+        ?
+        <button
+          type="submit"
+          data-testid="search-top-btn"
+          onClick={ handleClick }
+        >
+          <img
+            src={ searchIcon }
+            alt=""
+          />
+        </button>
+        :
+        <button
+          type="submit"
+          data-testid="search-top-btn"
+          onClick={ handleClick }
+        >
+          <img
+            src={ searchIcon }
+            alt=""
+          />
+        </button>
+        <h1> Colocar componente da barra de pesquisa </h1>
         <h1 data-testid="page-title">{ title }</h1>
       </div>
     );
   }
   return (
     <div>
-      <img src={ profileIcon } alt="" data-testid="profile-top-btn" />
+      <Link to="/">
+        <img src={ profileIcon } alt="" data-testid="profile-top-btn" />
+      </Link>
       <h1 data-testid="page-title">{ title }</h1>
     </div>
   );
