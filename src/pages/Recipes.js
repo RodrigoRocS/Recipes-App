8e6aa3afb355985b1e2c8721a26f38e7c2b91a71
lucas sanfelice <115578920+Lucas-Sanfelice@users.noreply.github.com>
2023-05-10 03:29:43 -0300

    requisito 19 e 21 parcial

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
 
 function Recipes() {
@@ -7,7 +7,10 @@ function Recipes() {
   const location = history.location.pathname;
   const [fetchData, data, isLoading] = useFetch([]);
   const [recipeArrays, setRecipeArrays] = useState({ firstTwelve: [], remaining: [] });
-
+  // const [isSearch, setIsSearch] = useState({ isSearching: false });
+  const [categoryArray, setCategoryArray] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const maxcategory = 4;
   const handleArray = useCallback((arr) => {
     const MN = 12;
     const recipeType = location === '/meals' ? 'meals' : 'drinks';
@@ -20,22 +23,57 @@ function Recipes() {
     return { firstTwelve, remaining };
   }, [location]);
 
+  const handleCategoryClick = (arg) => {
+    console.log(arg);
+    setSelectedCategory(arg);
+  };
   useEffect(() => {
-    if (location === '/drinks') {
-      fetchData('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
-    }
-    if (location === '/meals') {
-      fetchData('https://www.themealdb.com/api/json/v1/1/search.php?s=');
-    }
+    const fetchCategoryList = async () => {
+      if (location === '/drinks') {
+        await fetchData('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+        const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
+        const data2 = await response.json();
+        setCategoryArray(data2.drinks);
+      }
+      if (location === '/meals') {
+        const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
+        const data2 = await response.json();
+        setCategoryArray(data2.meals);
+        fetchData('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+      }
+    };
+
+    fetchCategoryList();
   }, [fetchData, location]);
 
   useEffect(() => {
     const resp = handleArray(data);
+    console.log(resp);
     setRecipeArrays(resp);
   }, [handleArray, data]);
 
   return (
     <div>
+      <button
+        onClick={ () => handleCategoryClick('all') }
+        data-testid="All-category-filter"
+      >
+        All
+      </button>
+      {
+        isLoading ? <p />
+          : categoryArray.length > 0 && categoryArray.map((item, index) => (
+            index <= maxcategory && (
+              <button
+                onClick={ () => handleCategoryClick(`${item.strCategory}`) }
+                key={ index }
+                data-testid={ `${item.strCategory}-category-filter` }
+              >
+                {item.strCategory}
+              </button>
+            )
+          ))
+      }
       {
         isLoading ? <>loading</>
           : recipeArrays.firstTwelve.length > 0
@@ -46,8 +84,7 @@ function Recipes() {
             >
               {
                 location === '/meals' ? (
-
-                  <>
+                  <Link to={ `/meals/${item.idMeal}` }>
                     <img
                       id={ index }
                       src={ item.strMealThumb }
@@ -55,12 +92,9 @@ function Recipes() {
                       data-testid={ `${index}-card-img` }
                     />
                     <div data-testid={ `${index}-card-name` }>{item.strMeal}</div>
-
-                  </>
-
+                  </Link>
                 ) : (
-
-                  <>
+                  <Link to={ `/drinks/${item.idDrink}` }>
                     <img
                       id={ index }
                       src={ item.strDrinkThumb }
@@ -68,9 +102,7 @@ function Recipes() {
                       data-testid={ `${index}-card-img` }
                     />
                     <div data-testid={ `${index}-card-name` }>{item.strDrink}</div>
-
-                  </>
-
+                  </Link>
                 )
               }
             </div>
