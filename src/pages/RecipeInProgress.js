1be22d4bbdaba4ab4aae1369867b727b223e54c7
vitorGaia <vitorgaia0027@gmail.com>
2023-05-15 14:23:27 -0300

    95% do css da recipes in progress

--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-max-depth */
 import React, { useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import RecipeInProgressContext from '../contexts/RecipeInProgressContext';
@@ -5,6 +6,7 @@ import IngredientsList from '../components/IngredientsList';
 import shareIcon from '../images/shareIcon.svg';
 import favoriteIcon from '../images/whiteHeartIcon.svg';
 import unfavoriteIcon from '../images/blackHeartIcon.svg';
+import '../styles/RecipeInProgress.css';
 
 function RecipeInProgress() {
   const {
@@ -40,47 +42,58 @@ function RecipeInProgress() {
   };
 
   return (
-    <div>
-      <div>
+    <div className="recipe-in-progress-page-container">
+      <header
+        className="recipe-in-progress-header-container"
+        style={ { backgroundImage: `url(${strMealThumb || strDrinkThumb})` } }
+      >
         <img
           data-testid="recipe-photo"
           alt="recipe-ilustration"
           src={ strMealThumb || strDrinkThumb }
         />
-        <h2 data-testid="recipe-title">{ strMeal || strDrink }</h2>
-        <span data-testid="recipe-category">{ strCategory }</span>
-        {copied && <span>Link copied!</span>}
-        <div>
+        <div className="recipe-in-progress-header-btns-container">
+          <h2 data-testid="recipe-title">{ strMeal || strDrink }</h2>
 
-          <button
-            data-testid="share-btn"
-            onClick={ handleShareRecipes }
-          >
-            <img src={ shareIcon } alt="share icon" />
-          </button>
+          <nav>
+            <span data-testid="recipe-category">{ strCategory }</span>
 
-          <button
-            data-testid="favorite-btn"
-            onClick={ handleFavoriteRecipes }
-            src={ isFavorite ? unfavoriteIcon : favoriteIcon }
-          >
-            {isFavorite
-              ? <img src={ unfavoriteIcon } alt="unfavorite icon" />
-              : <img src={ favoriteIcon } alt="favorite icon" />}
-          </button>
+            {copied && <span>Link copied!</span>}
+
+            <div>
+              <button
+                data-testid="share-btn"
+                onClick={ handleShareRecipes }
+              >
+                {shareIcon && <img src={ shareIcon } alt="share icon" />}
+              </button>
+
+              <button
+                data-testid="favorite-btn"
+                onClick={ handleFavoriteRecipes }
+                src={ isFavorite ? unfavoriteIcon : favoriteIcon }
+              >
+                {isFavorite
+                  ? <img src={ unfavoriteIcon } alt="unfavorite icon" />
+                  : <img src={ favoriteIcon } alt="favorite icon" />}
+              </button>
+            </div>
+          </nav>
         </div>
-      </div>
+      </header>
 
-      <div>
-        Ingredients
+      <div className="recipe-in-progress-ingredients-container">
+        <h3>Ingredients</h3>
         <IngredientsList />
       </div>
 
-      <div>
+      <div className="recipe-in-progress-instructions-container">
+        <h3>Instructions</h3>
         <span data-testid="instructions">{ strInstructions }</span>
       </div>
 
       <button
+        className="finish-recipe-btn"
         data-testid="finish-recipe-btn"
         disabled={ !checkboxesValue.every((check) => check === true) }
         onClick={ handleFinshRecipe }
